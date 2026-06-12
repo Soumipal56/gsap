25848190b9d7bc07153fb0387ed90219731a594b
Soumipal56 <soumiisc2020@gmail.com>
2026-06-12 18:47:36 +0530

    feat: implement GSAP timeline animation with playback control event listeners

--- script.js
+++ script.js
@@ -10,21 +10,45 @@
 //   }
 // });
 
-// const tl = gsap.timeline()
-
-// tl.to("#box1",{
-//   x:1500,
-//   rotate:360,
-//   duration:1.5,
-//   delay:1
-// })
-
-// tl.to("#box2",{
-//   x:1500,
-//   duration:1.5
-// })
-
 const play = document.querySelector('.play')
 const pause = document.querySelector('.pause')
 const restart = document.querySelector('.restart')
 const reverse = document.querySelector('.reverse')
+
+const tl = gsap.timeline({
+  paused: true
+})
+
+tl.to("#box1",{
+  x: () => window.innerWidth - 250,
+  rotate:360,
+  duration:1.5,
+  delay:1
+})
+
+tl.to("#box2",{
+  x: () => window.innerWidth - 250,
+  duration:1.5
+})
+
+tl.to("#box3",{
+  x: () => window.innerWidth - 250,
+  duration:1.5
+})
+
+play.addEventListener("click", () => {
+  tl.play();
+})
+
+pause.addEventListener("click", () => {
+  tl.pause();
+})
+
+restart.addEventListener("click", () => {
+  tl.restart();
+})
+
+reverse.addEventListener("click", () => {
+  tl.reverse();
+})
+
