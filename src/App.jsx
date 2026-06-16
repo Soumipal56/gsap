0fc155d2bbb7fad53155d7ab795a3b9636a2b585
Soumipal56 <soumiisc2020@gmail.com>
2026-06-16 12:07:06 +0530

    feat: add motion(framer motion) dependency and implement motion component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import AnimateonX from './components/AnimateonX'
+import { motion } from 'motion/react'
 
 const App = () => {
-  const boxRef = useRef([])
-  const containerRef = useRef(null)
-
-  const addToRefs = (el) => {
-    if (el && !boxRef.current.includes(el)) {
-      boxRef.current.push(el)
-    }
-  }
-
-  const {contextSafe} = useGSAP(()=>{
-    gsap.to(boxRef.current,{
-      x: 700,
-      duration: 1,
-      delay: 0.5,
-      stagger: 0.2
-    })
-  },{ scope: containerRef.current, dependencies: [], revertOnUpdate: true })
-  
+ 
   return (
-    <div ref={containerRef}>
-      <div ref={addToRefs} className='box'></div>
-      <div ref={addToRefs} className='box'></div>
-      <div ref={addToRefs} className='box'></div>
-      <AnimateonX><div className='box'></div></AnimateonX>
+    <div>
+      <motion.div transition={{ duration: 0.8, delay: 0.6 }} animate={{ x: 500 }} className="box"></motion.div>
     </div>
   )
 }
