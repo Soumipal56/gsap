ac7d1c4bb3c58506e543a422f9d2e9b319632116
Soumipal56 <soumiisc2020@gmail.com>
2026-06-16 11:29:07 +0530

    feat: initialize App component with GSAP-animated box elements using refs

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,29 @@ import gsap from 'gsap'
 import AnimateonX from './components/AnimateonX'
 
 const App = () => {
-  const boxRef = useRef(null)
+  const boxRef = useRef([])
   const containerRef = useRef(null)
+
+  const addToRefs = (el) => {
+    if (el && !boxRef.current.includes(el)) {
+      boxRef.current.push(el)
+    }
+  }
 
   const {contextSafe} = useGSAP(()=>{
     gsap.to(boxRef.current,{
       x: 700,
       duration: 1,
       delay: 0.5,
+      stagger: 0.2
     })
-  },{ scope: containerRef.current, dependencies: [], revertOnUpdate: true },
-)
+  },{ scope: containerRef.current, dependencies: [], revertOnUpdate: true })
   
   return (
     <div ref={containerRef}>
-      <div ref={boxRef} className='box'></div>
+      <div ref={addToRefs} className='box'></div>
+      <div ref={addToRefs} className='box'></div>
+      <div ref={addToRefs} className='box'></div>
       <AnimateonX><div className='box'></div></AnimateonX>
     </div>
   )
